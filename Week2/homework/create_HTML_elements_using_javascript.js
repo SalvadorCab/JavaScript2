3348a06fec9abe2673e406b780140823a2be196c
daniel <danielkapeleti@outlook.com>
2020-01-29 19:12:13 +0200

    These are files for Javascript2 week 2 homework

---
+++ Week2/homework/create_HTML_elements_using_javascript.js
@@ -0,0 +1,5 @@
+let newElement1 = document.createElement('p');
+let newElement2 = document.createElement('ul');
+let newElement3 = document.createElement('li');
+let newElement4 = document.createElement('img');
+console.log(newElement1,newElement2,newElement3,newElement4);
