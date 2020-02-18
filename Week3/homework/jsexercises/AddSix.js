ef3e583d79a26e610f3ad01afa944b4dacce0883
SalvadorCab <44257545+SalvadorCab@users.noreply.github.com>
2020-02-18 10:20:06 -0600

    Create AddSix.js

---
+++ Week3/homework/jsexercises/AddSix.js
@@ -0,0 +1,6 @@
+
+// Write a function called removeDuplicates, that takes in an array as an argument:
+// The function should remove duplicate letters. So the result should be:
+
+const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
+
